42f50456bfdfd7eb93aba6fddff61cfe1eb21c37
Ankit <ankitchauhan21500@gmail.com>
2024-11-04 18:52:30 +0530

    optimized the image | used alpine image | used caching

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("server is running");
-})
 
 app.get("/" , (req , response)=>{
     response.send("api is working");
 })
+
+app.listen(3000, ()=>{
+    console.log("server is running on 3000");
+})  
